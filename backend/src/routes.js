53db6552513f7cabc16d043305713df9f3882b7d
alexandreerick <erickalexandrex@gmail.com>
2020-03-24 21:20:15 -0300

    backend finalizado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,25 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
+
+// Login
+routes.post('/sessions', SessionController.create);
 
 // Ongs
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+// Profile
+routes.get('/profile', ProfileController.index);
+
+// Incidents
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
